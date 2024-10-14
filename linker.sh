49fd1c9b436033b583406e26048bc5e17c03c1e8
Alexis Scobey <19824067+octoturt@users.noreply.github.com>
2024-10-13 21:34:51 -0400

    added links for additional items
    added idle, pydoc, and wheel

--- linker.sh
+++ linker.sh
@@ -2,3 +2,6 @@
 
 ln -s $HOMEBREW_PREFIX/bin/python3 ./python
 ln -s $HOMEBREW_PREFIX/bin/pip3 ./pip
+ln -s $HOMEBREW_PREFIX/bin/idle3 ./idle
+ln -s $HOMEBREW_PREFIX/bin/pydoc3 ./pydoc
+ln -s $HOMEBREW_PREFIX/bin/wheel3 ./wheel
